d9ab75ebec907c53e4ea47d820d32b4050d70094
Surendra Tholiya <114131620+suren015@users.noreply.github.com>
2023-10-11 02:08:02 +0530

    Add files via upload

---
+++ codsoft_num_guess.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    // Initialize random seed
+    srand(time(0));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = rand() % 100 + 1;
+
+    int userGuess;
+    int attempts = 0;
+
+    cout << "Welcome to the Guess My Number Game!\n" << endl;
+    cout << "I've selected a number between 1 and 100. Try to guess it.\n" << endl;
+
+    while (true)
+    {
+        cout << "Enter your guess :: ";
+        cin >> userGuess;
+        attempts++;
+
+        if (userGuess == randomNumber)
+        {
+            cout << "Congratulations! You've guessed the correct number, which is " << randomNumber << "." << endl;
+            cout << "It took you " << attempts << " attempts to guess it." << endl;
+            break;
+        }
+        else if (userGuess < randomNumber)
+        {
+            cout << "Try a higher number." << endl;
+        }
+        else
+        {
+            cout << "Try a lower number." << endl;
+        }
+    }
+
+    system("PAUSE");
+    return 0;
+}
